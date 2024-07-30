e364870e35253283aa30538cf42051a8c23e2e90
Christan Shane Plaza <christanshaneplaza@gmail.com>
2024-07-30 14:11:30 +0800

    Initial Commit

---
+++ IAmRich/IAmRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichApp.swift
+//  IAmRich
+//
+//  Created by Sun Asterisk Philippines on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
